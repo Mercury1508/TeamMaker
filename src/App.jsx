be60687fa7d9b08e556dce6b11fb7bd403a06ed2
ethan <deepaklewiskumar.19@gmail.com>
2022-06-18 17:26:14 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './Components/Home/Home.jsx'
+import Login from './Components/SignIn/Login.jsx';
+import Register from './Components/SignIn/Register.jsx';
+
+
+
+function App(){
+    return(<>
+    <Home />
+    </>
+    )
+}
+
+export default App
